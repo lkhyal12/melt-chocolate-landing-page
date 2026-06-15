db20860de326b7ea58d24b54b6722c1deda77739
lkhyal <lkhyal.ab@gmail.com>
2026-06-15 23:53:44 +0200

    done with the paralex effect and the footer section

--- src/sections/Paralex.jsx
+++ src/sections/Paralex.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Chocolate from "../assets/images/choclate-bg-2.png";
 const Paralex = () => {
-  return <div></div>;
+  return (
+    <section className="inner-container h-screen py-20">
+      <div
+        className="h-full rounded-xl bg-fixed bg-center bg-cover"
+        style={{ backgroundImage: `url(${Chocolate})` }}
+      />
+    </section>
+  );
 };
 
 export default Paralex;

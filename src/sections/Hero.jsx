5a729b7b9952988ff88ffddda94f5166d2e7e079
lkhyal <lkhyal.ab@gmail.com>
2026-06-16 00:06:30 +0200

    fixing some animation bugs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import HeroChocolates from "./HeroChocolates";
 import FlavorCards from "./FlavorCards";
 import gsap from "gsap";
@@ -8,63 +8,67 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [activeFlavores, setActiveFlavores] = useState("");
-
-  useGSAP(() => {
-    const pairs = [
-      {
-        choco: document.querySelector(".caremel-choco"),
-        card: document.querySelector(".caramel-card"),
-      },
-      {
-        choco: document.querySelector(".cocoa-choco"),
-        card: document.querySelector(".cocoa-card"),
-      },
-      {
-        choco: document.querySelector(".orange-choco"),
-        card: document.querySelector(".orange-card"),
-      },
-      {
-        choco: document.querySelector(".almond-choco"),
-        card: document.querySelector(".almond-card"),
-      },
-    ];
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".choco-section",
-        start: "top top",
-        end: "70% center",
-        scrub: true,
-        // markers: true,
-        // pin: true,
-        invalidateOnRefresh: true,
-      },
-    });
-
-    pairs.forEach(({ choco, card }) => {
-      tl.to(
-        choco,
+  const heroRef = useRef();
+  useGSAP(
+    () => {
+      const pairs = [
         {
-          x: () => {
-            const c = choco.getBoundingClientRect();
-            const t = card.getBoundingClientRect();
-            const calc = t.left + t.width / 2 - (c.left + c.width / 2);
-            return calc;
-          },
-          y: () => {
-            const c = choco.getBoundingClientRect();
-            const t = card.getBoundingClientRect();
-            const calc = t.top + t.height / 2 - (c.top + (c.height / 2) * 1.2);
-            return calc;
-          },
-          scale: 0.6,
-          ease: "none",
+          choco: document.querySelector(".caremel-choco"),
+          card: document.querySelector(".caramel-card"),
         },
-        0,
-      );
-    });
-  }, []);
+        {
+          choco: document.querySelector(".cocoa-choco"),
+          card: document.querySelector(".cocoa-card"),
+        },
+        {
+          choco: document.querySelector(".orange-choco"),
+          card: document.querySelector(".orange-card"),
+        },
+        {
+          choco: document.querySelector(".almond-choco"),
+          card: document.querySelector(".almond-card"),
+        },
+      ];
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".choco-section",
+          start: "top top",
+          end: "70% center",
+          scrub: true,
+          // markers: true,
+          // pin: true,
+          invalidateOnRefresh: true,
+        },
+      });
+
+      pairs.forEach(({ choco, card }) => {
+        tl.to(
+          choco,
+          {
+            x: () => {
+              const c = choco.getBoundingClientRect();
+              const t = card.getBoundingClientRect();
+              const calc = t.left + t.width / 2 - (c.left + c.width / 2);
+              return calc;
+            },
+            y: () => {
+              const c = choco.getBoundingClientRect();
+              const t = card.getBoundingClientRect();
+              const calc =
+                t.top + t.height / 2 - (c.top + (c.height / 2) * 1.2);
+              return calc;
+            },
+            scale: 0.6,
+            ease: "none",
+          },
+          0,
+        );
+      });
+    },
+    { scope: heroRef.current },
+  );
   return (
-    <section className="choco-section inner-container">
+    <section ref={heroRef} className="choco-section inner-container">
       <HeroChocolates activeFlavor={activeFlavores} />
       <FlavorCards setActiveFlavore={setActiveFlavores} />
     </section>
